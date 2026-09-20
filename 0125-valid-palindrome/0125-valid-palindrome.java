120db37f94ebb7c7103e0117a4fccc279626e339
HINDHUJA <itsdreamer712@gmail.com>
2026-09-20 11:19:35 +0530

    Time: 5 ms (36.42%), Space: 44.9 MB (38.69%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       StringBuilder sb=new StringBuilder();
+       for(int i=0;i<s.length();i++)
+       {
+        char ch=s.charAt(i);
+         if(Character.isLetterOrDigit(ch))
+         {
+            sb.append(Character.toLowerCase(ch));
+         }
+       }
+       StringBuilder sb2=new StringBuilder(sb);
+       sb2.reverse();
+       return sb.toString().equals(sb2.toString()); 
+    }
+}
